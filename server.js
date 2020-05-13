a0c6ac93c3832c5a8067cb97a95c560765227225
Kaleb Raymond <43358126+KalebRaymond@users.noreply.github.com>
2020-05-13 19:49:47 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const db = require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
-const alpha = require('alphavantage')({ key: '****' });
+const alpha = require('alphavantage')({ key: process.env.AV_KEY });
 const nodeMailer = require('nodeMailer'); 
 const fs = require('fs'); 
 const parser = require('node-html-parser').parse;
@@ -9,61 +10,68 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 
-/* Email */
+const user =
+{
+	curPortfolio: {},
+	curClosePrices: {},
+	dailyStockAlertFlags: {}
+};
+
+/* Node Mailer */
 var transporter = nodeMailer.createTransport({
 	service: 'gmail',
 	auth: 
 	{
 		user: 'StockWatcherNotification@gmail.com',
-		pass: '****'
+		pass: process.env.NODEMAIL_PW
 	}
 });
 
-//Checks current market price of every stock in user's portfolio. When a stock's price is above the desired price, an e-mail with all stocks above desired price will be sent to user
-var curPortfolio = {};
-var curOpenPrices = {};
-var dailyStockAlertFlags = {};
+/* 	
+/	Checks current market price of every stock in user's portfolio. When a 
+/	stock's price is above the user's desired price, it is added to a string
+/	that is then emailed to the user. 
+*/
 async function notify()
 {
-	curOpenPrices = {};
-	var emailFlag = 0;
+	user.curClosePrices = {};
+	var sendEmailFlag = 0;
 	var emailSubject = '';
 	var emailBody = 'Automated response. ';
-	var openPrice = 0;
+	var closePrice = 0;
 	
-	for(var s in curPortfolio)
+	for(var s in user.curPortfolio)
 	{
-		//Only check stocks that haven't already been in an e-mail in the last hour (or since last e-mail refresh)
-		if(dailyStockAlertFlags[s] === undefined)
+		//Only check stocks that haven't already been in an e-mail in the since last e-mail refresh
+		//to avoid spamming the user with multiple emails
+		if(user.dailyStockAlertFlags[s] === undefined)
 		{
 			await alpha.data.intraday(s, 'compact', 'json', '5min').then(data => 
 			{
-				var openPrice = Number(get_open(data));
-				curOpenPrices[s] = openPrice;
+				var closePrice = Number(get_close_price(data));
+				user.curClosePrices[s] = closePrice;
 			});
 		}
 	}
 	
-	for(var s in curOpenPrices)
+	for(var s in user.curClosePrices)
 	{
-		//console.log('open ' + s + " " + curOpenPrices[s] + " " + curPortfolio[s]);
-		if(curOpenPrices[s] > curPortfolio[s])
+		if(user.curClosePrices[s] > user.curPortfolio[s])
 		{
-			var emailFlag = 1;
+			var sendEmailFlag = 1;
 			var emailSubject = emailSubject + s + ', ';
-			var emailBody = "<br>" + emailBody + s + ' is now at $' + curOpenPrices[s] + '! ';
-			dailyStockAlertFlags[s] = 1;
+			var emailBody = '\n' + emailBody + s + ' is now at $' + user.curClosePrices[s] + '! ';
+			user.dailyStockAlertFlags[s] = 1;
 		}
 	}
 	
-	//console.log(emailFlag);
-	if(emailFlag)
+	if(sendEmailFlag)
 	{
 		emailSubject = emailSubject.substring(0, emailSubject.length - 2) + ' alert!'
 		
 		var mailOptions = {
 			from: 'StockWatcherNotification@gmail',
-			to: 'k.raymond.form@gmail.com',
+			to: process.env.TO_EMAIL,
 			subject: emailSubject,
 			text: '',
 			html: emailBody
@@ -83,15 +91,77 @@ async function notify()
 	}
 }
 
+/*
+/	Resets all values in user.dailyStockAlertFlags to false. This allows stocks that
+/	have already been in an email since the last refresh to be in another email.
+/	If a stock hits the desired price and stays above it, then the user would get
+/	an email about that stock every five minutes. 
+*/
 function clear_alert_flags()
 {
-	dailyStockAlertFlags = {};
+	user.dailyStockAlertFlags = {};
 }
 
 //setInterval(notify, 5 * 60 * 1000);
 //setInterval(clear_alert_flags, 60 * 60 * 1000);
 
-/* Localhost interactivity */
+/*
+/	Returns the most recent close price for a stock from a given alpha vantage 
+/	JSON object. Technically it's called a closing price but I already wrote
+/	'close' so many times replacing it would be a pain.
+*/
+function get_close_price(stockData)
+{
+	for (var i in stockData['Time Series (5min)'])
+	{
+		//Return the first JSON entry in 'Time Series (5min)'
+		return stockData['Time Series (5min)'][i]['4. close']; 
+	}
+}
+
+function add_to_portfolio(stock, desiredPrice, curClose)
+{
+	user.curPortfolio[stock] = desiredPrice;
+	user.curClosePrices[stock] = curClose;
+}
+
+function remove_from_portfolio(stock)
+{
+	delete user.curPortfolio[stock];
+	delete user.curClosePrices[stock]
+}
+
+/*
+/	Updates and renders the 'Your Portfolio' section of index.ejs by
+/	overwriting the table tag contents
+*/
+function render_portfolio(req, res)
+{
+	var html = fs.readFileSync('views/index.ejs');
+	const root = parser(html, {script: true});
+	const body = root.querySelector('table');
+	
+	var portfolioText = '';
+	for(var s in user.curPortfolio)
+	{
+		var openPrice = '$' + user.curClosePrices[s];
+		if(user.curClosePrices[s] == undefined) openPrice = "Awaiting data...";
+		
+		portfolioText += '\n\t\t\t\t\t\t\t<tr class="stock" id="' + s + '" onmouseover="hover_event(\'' + s + '\')">'
+			+ '<td id="symbol">' + s + '</td>'
+			+ '<td id="close">Last price: ' + openPrice + '</td>'
+			+ '<td id="watch">Watching for: $' + user.curPortfolio[s] + '</td>'
+			+ '<td id="delete"><button form="tableForm" class="deleteButton" type="submit" onclick="sendSymbol(\'' + s + '\')">DELETE</button></td></tr>';
+	}
+	portfolioText += '\n\t\t\t\t\t\t';
+	body.set_content(portfolioText);
+	
+	fs.writeFileSync('views/index.ejs', root.toString());
+	res.render('index', {});
+}
+
+
+/* Express middleware */
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
@@ -101,56 +171,6 @@ app.get('/', function (req, res)
 	notify(); //Retrieving stock data is slow, if you await notify the page wont load for a few seconds
 	render_portfolio(req, res);
 })
-
-function get_open(stockData)
-{
-    var dataString = JSON.stringify(stockData);
-    var objectValue = JSON.parse(dataString);
-	
-	/* Access most recent open price from JSON data */
-	for (var i in objectValue['Time Series (5min)'])
-	{
-		for(var j in objectValue['Time Series (5min)'][i])
-		{
-			return objectValue['Time Series (5min)'][i][j]; //Most recent price ('open price') (should actually be 'closed price'...)
-			//break;
-		}
-		//break;
-	}
-}
-
-function add_to_portfolio(stock, desiredPrice, curOpen)
-{
-	curPortfolio[stock] = desiredPrice;
-	curOpenPrices[stock] = curOpen;
-}
-
-function remove_from_portfolio(stock)
-{
-	delete curPortfolio[stock];
-	delete curOpenPrices[stock]
-}
-
-function render_portfolio(req, res)
-{
-	var html = fs.readFileSync('views/index.ejs');
-	const root = parser(html, {script: true});
-	const body = root.querySelector('table');
-	
-	var portfolioText = '';
-	for(var s in curPortfolio)
-	{
-		var openPrice = '$' + curOpenPrices[s];
-		if(curOpenPrices[s] == undefined) openPrice = "Awaiting data...";
-		
-		portfolioText += '\n\t\t\t\t\t\t<tr class="stock" id="' + s + '" onmouseover="hover_event(\'' + s + '\')"><td>' + s + '</td><td>Last price: ' + openPrice + '</td><td>Watching for: $' + curPortfolio[s] + '</td><td><button form="tableForm" class="deleteButton" type="submit" onclick="sendSymbol(\'' + s + '\')">DELETE</button></td></tr>';
-	}
-	portfolioText += '\n\t\t\t\t\t';
-	body.set_content(portfolioText);
-	
-	fs.writeFileSync('views/index.ejs', root.toString());
-	res.render('index', {});
-}
 
 app.post('/', 
 	/* Function for adding stock to portfolio, accessed via root path with query ?name=formRemove  */
@@ -171,10 +191,11 @@ app.post('/',
 			{
 				alpha.data.intraday(symbol, 'compact', 'json', '5min').then(data => 
 				{
+					console.log(data);
 					fs.writeFileSync('time_series_data.json', JSON.stringify(data)); //Make this async?
 					
-					var openPrice = get_open(data);
-					add_to_portfolio(symbol, flDesiredPrice, openPrice);
+					var closePrice = get_close_price(data);
+					add_to_portfolio(symbol, flDesiredPrice, closePrice);
 					render_portfolio(req, res);
 
 					//Send e-mail if current open price is greater than desired price
@@ -199,6 +220,8 @@ app.post('/',
 					}*/
 				}).catch(function(err)
 				{
+					//Error occurs if user enters a stock symbol that the API doesn't recognize
+					//but also if the API's call limit is exceeded (5/min, 500/day)
 					console.log('Alpha Vantage error: stock ' + symbol + ' not found.');
 					render_portfolio(req, res);
 				});
@@ -214,9 +237,7 @@ app.post('/',
 	{
 		if (req.query.name === 'formRemove')
 		{
-			//console.log('Attempting to remove ' + req.body.removedSymbol.toUpperCase() + ' from portfolio');
-			//console.log('Arrived at second function');
-			console.log(req.body);
+			//console.log(req.body);
 			remove_from_portfolio(req.body.removedSymbol.toUpperCase());
 			render_portfolio(req, res);
 		}
@@ -225,25 +246,13 @@ app.post('/',
 			next();
 		}
 	},
-	/* Function for exporting portfolio as json file, accessed via root path with query ?name=formExport */
+	/* Function for exporting portfolio as JSON file, accessed via root path with query ?name=formExport */
 	function(req, res, next)
 	{
 		if (req.query.name === 'formExport')
 		{
-			var portfolioJSON = JSON.stringify(curPortfolio);
+			var portfolioJSON = JSON.stringify(user.curPortfolio);
 			fs.writeFile('portfolio.json', portfolioJSON, function (err){if (err) throw err;} );
-		}
-		else
-		{
-			next();
-		}
-	},
-	/* Function for rendering line chart for a stock, accessed via root path with query ?name=graph */
-	function(req, res, next)
-	{
-		if( req.query.name === 'graph')
-		{
-			console.log(req.body.removedSymbol); //Should change the name of removedSymbol in html...
 		}
 		else
 		{
@@ -251,11 +260,10 @@ app.post('/',
 		}
 	});
 
-
 /* Socket IO */
 http.listen(8080, function (req) 
 {
-	console.log('Example app listening on port 8080!')
+	console.log('Stock Watcher listening on port 8080')
 	fs.readFile('portfolio.json', function(err, data) 
 	{
 		if(err)
@@ -265,23 +273,23 @@ http.listen(8080, function (req)
 		else
 		{
 			console.log('portfolio.json successfully opened.');
-			curPortfolio = JSON.parse(data); 
+			user.curPortfolio = JSON.parse(data); 
 		}
 	});
 })
 
+/*
+/	Socket io handles 'hover' event when user hovers over a table row in
+/	the portfolio. io emits a 'draw' signal back telling the client to render
+/	a line chart for the corresponding stock.
+*/
 io.on('connection', (socket) => {
-	console.log('Socket works');
-	socket.on('clicked', function()
-	{
-		console.log("Test button clicked");
-	});
+	//console.log('Socket works');
 	
 	socket.on('hover', (symbol) =>
 	{
 		console.log('Hovering over ' + symbol);
-		//Ok ok ok. Get arrays in this function right here. Emit them back to index.ejs and call draw_graph()
-		alpha.data.intraday(symbol, 'compact', 'json', '5min').then(data =>
+		/*alpha.data.intraday(symbol, 'compact', 'json', '5min').then(data =>
 		{
 			var timesStack = [];
 			var pricesStack = [];
@@ -310,7 +318,7 @@ io.on('connection', (socket) => {
 			};
 			
 			io.emit('draw', draw_params);
-		});
+		});*/
 	});
 });
 
